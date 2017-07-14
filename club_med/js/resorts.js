96970f3107f6a1bfae1e78833635408ff8718189
Vadim Semenets <semvadser@gmail.com>
2017-07-14 14:09:29 +0300

    Club Med

---
+++ club_med/js/resorts.js
@@ -0,0 +1,12 @@
+var resorts = {
+  sun: {
+    'Asia': ['Asia Resort 1', 'Asia Resort 2', 'Asia Resort 3'],
+    'Indonesia': ['Indonesia Resort 1', 'Indonesia Resort 2', 'Indonesia Resort 3']
+  },
+
+  snow: {
+    'Snow country 1': ['Snow Country 1 -  Resort 1', 'Snow Country 1 -  Resort 2'],
+    'Snow country 2': ['Snow Country 2 -  Resort 1', 'Snow Country 2 -  Resort 2'],
+    'Snow country 3': ['Snow Country 3 -  Resort 1', 'Snow Country 3 -  Resort 2', 'Snow Country 3 -  Resort 3', 'Snow Country 3 -  Resort 4'],
+  }
+}
