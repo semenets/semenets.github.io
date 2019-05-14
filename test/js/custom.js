8e9af86b3d89f05a5bcdda2c1166abc3307c74fc
Vadim Semenets <semvadser@gmail.com>
2019-05-14 10:05:17 +0300

    test

--- test/js/custom.js
+++ test/js/custom.js
@@ -88,7 +88,6 @@ $(function(){
 var handler = function(){
 	var height_footer = $('footer').height();	
 	var height_header = $('header').height();		
-	//$('.content').css({'padding-bottom':height_footer+40, 'padding-top':height_header+40});
 	var viewport_wid = viewport().width;
 	var viewport_height = viewport().height;
 	
