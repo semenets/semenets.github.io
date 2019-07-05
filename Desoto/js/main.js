a4ba119efa99a585f5156527a9f0f7ef7c49bfb3
Vadim Semenets <semvadser@gmail.com>
2019-07-05 12:23:40 +0300

    page

--- Desoto/js/main.js
+++ Desoto/js/main.js
@@ -7,10 +7,6 @@ $(function(){
 	});
 });
 
-// $(document).ready(function() {
-// 	$("body").children().each(function() {
-// 		$(this).html($(this).html().replace(/&#8232;/g," "));
-// 	});
-// });
-
-window.onload = function() { document.body.innerHTML = document.body.innerHTML.replace(/\u2028/g, ''); }
+window.onload = function() { 
+	document.body.innerHTML = document.body.innerHTML.replace(/\u2028/g, '')
+};
